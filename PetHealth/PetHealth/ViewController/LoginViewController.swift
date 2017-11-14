4ddff440b8a4aec097d85ed6c85cf04d1056b94d
Francis marquez <u201421465@upc.edu.pe>
2017-11-14 04:15:57 -0800

    Login view ÷ outlets

--- PetHealth/PetHealth/ViewController/LoginViewController.swift
+++ PetHealth/PetHealth/ViewController/LoginViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!	
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
