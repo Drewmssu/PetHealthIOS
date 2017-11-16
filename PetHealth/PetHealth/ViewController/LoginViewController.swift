75020f3af1cdb362350981768752e8bbca0959b4
Gonzalo Cirilo Herrera <gonzalociriloherrera@Gonzalos-Mac.local>
2017-11-16 15:11:00 -0500

    login fixed

--- PetHealth/PetHealth/ViewController/LoginViewController.swift
+++ PetHealth/PetHealth/ViewController/LoginViewController.swift
@@ -9,9 +9,8 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    
     @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!	
+    @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
