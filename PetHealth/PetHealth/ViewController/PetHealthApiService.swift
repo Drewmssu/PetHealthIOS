2589f009149eed5996d8cf0b1102ed58756a60a6
palvarezm <u201516795@upc.edu.pe>
2017-11-20 12:19:24 -0500

    Create SingupViewController & Complete PetHealthApiService

---
+++ PetHealth/PetHealth/ViewController/PetHealthApiService.swift
@@ -0,0 +1,22 @@
+//
+//  PetHealthApiService.swift
+//  PetHealth
+//
+//  Created by Paul on 11/20/17.
+//  Copyright © 2017 iSoftware. All rights reserved.
+//
+
+import Foundation
+
+public class PetHealthApiService {
+    static let PET_URL: String = "http://pethealth.azurewebsites.net/user/pets";
+    static let TIP_URL: String = "http://pethealth.azurewebsites.net/tips";
+    static let LOGIN_URL: String = "http://pethealth.azurewebsites.net/user/login";
+    static let DOCTYPE_URL: String = "http://pethealth.azurewebsites.net/documenttype";
+    static let SIGNUP_USER: String = "http://pethealth.azurewebsites.net/user/register";
+    static let SIGNUP_CUSTOMER: String = "http://pethealth.azurewebsites.net/user/registerCustomer";
+    static let VETERINARY_URL: String = "http://pethealth.azurewebsites.net/veterinaries";
+    static let CUSTOMER_URL: String = "http://pethealth.azurewebsites.net/customers";
+    static let ADD_PET_URL: String = "http://pethealth.azurewebsites.net/pet/add";
+    static let APPOINTMENT_URL: String = "http://pethealth.azurewebsites.net/pets/appointments";
+}
---
+++ PetHealth/PetHealth/ViewController/SignupViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SignupViewController.swift
+//  PetHealth
+//
+//  Created by Paul on 11/20/17.
+//  Copyright © 2017 iSoftware. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+func sendUser(){
+    Alamofire.request()
+}
